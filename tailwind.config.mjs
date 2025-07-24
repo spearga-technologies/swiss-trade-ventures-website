68738274d1e2f94ba074faef792f00cb960fe06f
spearga-technologies <speargatech@gmail.com>
2025-07-24 14:05:42 +0530

    Start repository

---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#0003ff',
+          100: '#cce6ff',
+          200: '#99cfff',
+          300: '#66b7ff',
+          400: '#339fff',
+          500: '#0003ff',
+          600: '#0003e6',
+          700: '#0003cc',
+          800: '#0003b3',
+          900: '#003d80',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
