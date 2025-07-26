c2da64f06355662ac9f1d6b058d046c3865a6c0c
spearga-technologies <speargatech@gmail.com>
2025-07-27 00:51:56 +0530

    Update Firebase configuration with correct API keys

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- apiKey: "AIzaSyADADzp8HDoX8dWs8OE2Iv_rWQT01yF8zQ",
+  apiKey: "AIzaSyADADzp8HDoX8dWs8OE2Iv_rWQT01yF8zQ",
   authDomain: "swisstradeventures-admin.firebaseapp.com",
   projectId: "swisstradeventures-admin",
   storageBucket: "swisstradeventures-admin.firebasestorage.app",
   messagingSenderId: "251484801619",
   appId: "1:251484801619:web:2b5418b63c39f7271514a6",
+  measurementId: "G-M59VCMVXVN"
 };
 
 // Initialize Firebase
